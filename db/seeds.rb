d7b57bc87d47d033fe8c25197e9cbfae0a6f21c4
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-26 11:26:04 -0300

    add categories to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,18 @@ puts %{
 }
 
 User.destroy_all
+Category.destroy_all
+Article.destroy_all
+
 
 puts "Create users"
 User.create! email: "player@one.com", password: '123456'
 User.create! email: "player@two.com", password: '123456'
 
+puts 'Create categories'
+Category.create! title: 'Category one'
+Category.create! title: 'Category two'
+
 puts "Create articles"
-Article.create! title: "one article", body: 'With a nice body', user: User.first
-Article.create! title: "Another article", body: 'With another nice body', user: User.second
+Article.create! title: "one article", body: 'With a nice body', user: User.first, category: Category.first
+Article.create! title: "Another article", body: 'With another nice body', user: User.second, category: Category.second
